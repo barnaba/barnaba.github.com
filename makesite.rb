74a72131b3e921ec7fe10313098ce82cae3b5530
Barnaba Turek <barnabaturek@gmail.com>
2011-10-27 00:42:20 +0200

    moved to erb

--- makesite.rb
+++ makesite.rb
@@ -1,83 +1,29 @@
 require 'rubygems'
 require 'mechanize'
+require 'erb'
 require 'ruby-debug'
+
+class Project
+  attr_accessor :url, :title, :description, :is_fork, :anchor
+
+  def initialize(elem)
+    link = elem.search('h3 a').first
+    @url, @title = 'http://github.com' + link.attributes['href'], link.text
+    @anchor = "<a href=\"#{@url}\">#{@title}</a>"
+    @description = elem.search('.description').first ? elem.search('.description').first.text : ""
+    @is_fork = !(elem.attributes['class'].text =~ /fork/).nil?
+  end
+
+  def to_s
+    puts [@url, @title, @description, @is_fork].join " | "
+  end
+end
   
 agent = Mechanize.new
 page = agent.get('http://github.com/barnaba')
-projects = page.search('.public')
-project_list =  []
-once = true
-projects.each do |project|
-  p = project.search('h3 a').first
-  name, href = p.text, p.attributes['href']
-  next if name == "barnaba.github.com"
-  project_link = ''
-  project_link += " <a href=\"http://github.com#{href}\">#{name}</a>"
+projects = page.search('.public').map { |elem| Project.new(elem) }
+projects = projects.select { |p| p.title != 'barnaba.github.com' }
 
-  if once 
-    once = false
-    project_link += ' <span class="label success">New</span>'
-  end
-
-  if project.attributes['class'].text =~ /fork/
-    project_link += ' <span class="label warning">Fork</span>'
-  end
-
-  description = project.search('.description').first
-  unless description.nil?
-    project_link += " <span class=\"description\">#{description.text}</span>"
-  end
-
-  project_list << "<li> #{project_link} </li>"
-end
-
-puts <<doc
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN"
-    "http://www.w3.org/TR/html4/strict.dtd">
-<html lang="en">
-  <head>
-    <meta http-equiv="content-type" content="text/html; charset=utf-8">
-    <title>barnaba</title>
-    <link rel="stylesheet" href="http://twitter.github.com/bootstrap/1.3.0/bootstrap.min.css">
-    <style type="text/css">
-    body {
-      padding-top : 10%;
-    }
-    #linkspam {
-      text-align: right;
-      list-style-type: none;
-      margin-right: 1em;
-    }
-
-    li {
-      line-height: 1.8em;
-    }
-
-    #linkspam li {
-      background-position: 100% .4em;
-      padding-right: .6em;
-    }
-    </style>
-  </head>
-  <body>
-    <div class="container">
-        <div class="span-one-third">
-        <ul id="linkspam">
-          <li><a rel="author" href="https://profiles.google.com/BarnabaTurek"> <img src="http://www.google.com/images/icons/ui/gprofile_button-44.png" width="36" height="36"> </a></li>
-          <li><a rel="author" href="http://www.reddit.com/r/barnaba"> <img src="http://www.reddit.com/static/blog_snoo.gif" width="36" height="49"> </a></li>
-          <li><a href="http://www.twitter.com/barnex"><img src="http://twitter-badges.s3.amazonaws.com/t_logo-c.png" alt="Follow barnex on Twitter"/></a></li>
-        </ul>
-        </div>
-      <div class="row">
-        <div class="span-one-third">
-          <h1 class="centered">Barnaba Turek</h1>
-          <h2> <a href="http://github.com/barnaba">My projects</a> </h1>
-          #{project_list.join("\n")}
-          </ul>
-        </div>
-      </div>
-    </div>
-  </body>
-</html>
-doc
-
+content = File.read('index.html.erb')
+template = ERB.new(content)
+File.open("index.html", 'w') { |f| f.puts(template.result) }
